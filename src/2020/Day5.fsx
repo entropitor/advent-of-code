b0f5a6f5d270186afcc421b95102a738caef2d18
Jens Claes <jens@datacamp.com>
2020-12-21 22:41:46 +0100

    feat: add day5.fsx

---
+++ src/2020/Day5.fsx
@@ -0,0 +1,45 @@
+#load "shared.fsx"
+open Shared
+open System
+
+let bsp chr (min,max) next =
+  if chr = next then
+    min+(max-min)/2+1, max
+  else
+    min, min+(max-min)/2
+
+let row line = line |> Seq.take 7 |> Seq.fold (bsp 'B') (0, 127) |> fst
+let column line = line |> Seq.skip 7 |> Seq.fold (bsp 'R') (0, 7) |> fst
+let seat line = (row line) * 8 + (column line)
+
+let maxSeat lines =
+  lines |> Seq.map seat |> Seq.max
+
+let missingSeat lines =
+  let seats = lines |> Seq.map seat |> Seq.sort
+  let zipped =
+    seats
+    |> Seq.append (seats |> Seq.head |> Seq.singleton)
+    |> Seq.zip seats
+  let before =
+    zipped
+    |> Seq.skipWhile (fun (y,x) -> y-x <> 2)
+    |> Seq.head
+    |> snd
+  before + 1
+
+(*
+let test = 
+  """BFFFBBFRRR
+FFFBBBFRRR
+BBFFBBFRLL""".Split("\n") |> Seq.ofArray
+test |> Seq.map row
+test |> Seq.map column
+test |> Seq.map seat
+*)
+
+(* A *)
+inputLines |> maxSeat |> printfn "%A"
+(* B *)
+inputLines |> missingSeat |> printfn "%A"
+
